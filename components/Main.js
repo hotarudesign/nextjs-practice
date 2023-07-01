eccbce35740aa1da09a9d8edeeaebadc1d32e230
hotarudesign <hotarudesign1220@gmail.com>
2023-07-01 16:44:21 +0900

    💄 スタイルファイルの分割

--- components/Main.js
+++ components/Main.js
@@ -1,46 +1,14 @@
 import { Headine } from "../components/Headline";
 import { Links } from "../components/Links";
-import styles from "../styles/Home.module.css";
+import classes from "./Main.module.css";
 
 export function Main(props) {
   return (
-    <main>
+    <main className={classes.main}>
       <Headine page={props.page}>
-        <code>pages/{props.page}.js</code>
+        <code className={classes.code}>pages/{props.page}.js</code>
       </Headine>
       <Links />
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </main>
   );
 }
