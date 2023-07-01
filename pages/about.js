89fb200806ba8e81744f5fa9fa582383183fe37a
hotarudesign <hotarudesign1220@gmail.com>
2023-07-01 11:18:55 +0900

    ✨ aboutページを追加

---
+++ pages/about.js
@@ -0,0 +1,60 @@
+import Head from "next/head";
+import { Links } from "../components/Links";
+import { Footer } from "../components/Footer";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>About Page</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className={styles.title}>About Page</h1>
+
+        <p className={styles.description}>
+          Get started by editing <code>pages/about.js</code>
+        </p>
+
+        <Links />
+      </main>
+
+      <Footer />
+
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
