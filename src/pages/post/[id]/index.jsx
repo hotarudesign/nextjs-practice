4d802d8d150ebdd26c56fc55bb0b1a71164e28b5
hotarudesign <hotarudesign1220@gmail.com>
2023-07-29 09:35:09 +0900

    🚚 ファイルシステムルーティングを試しました！

--- src/pages/post/[id]/index.jsx
+++ src/pages/post/[id]/index.jsx
@@ -1,17 +1,18 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { Header } from "src/components/Header";
-import { Posts } from "src/components/Posts";
 import styles from "src/styles/Home.module.css";
 
-const Home = () => {
+const PostId = () => {
+  const router = useRouter();
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      <Posts />
+      <div>{router.query.id}</div>
     </div>
   );
 };
-export default Home;
+export default PostId;
