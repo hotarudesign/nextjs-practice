d29ab4496eba8c71af44d15362b945d11b9b157d
hotarudesign <hotarudesign1220@gmail.com>
2023-07-29 11:14:35 +0900

    🔥 Indexから不要なコードを削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,32 +1,17 @@
 import Head from "next/head";
 import { Header } from "src/components/Header";
-import { Main } from "src/components/Main";
-import { Footer } from "src/components/Footer";
 import styles from "src/styles/Home.module.css";
 
-const About = (props) => {
+const Index = () => {
   return (
     <div className={styles.container}>
       <Head>
         <title>About Page</title>
       </Head>
       <Header />
-      <button onClick={props.handleClick}>ボタン</button>
-      <button onClick={props.handleDisplay}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-      {props.isShow ? <h2>{props.doubleCount}</h2> : null}
-
-      <input type="text" value={props.text} onChange={props.handleChange} />
-      <button onClick={props.handleAdd}>追加</button>
-      <ol>
-        {props.array.map((item) => {
-          return <li key={item}>{item}</li>;
-        })}
-      </ol>
-      <Main page="about" />
-      <Footer />
+      <h1>Next.jsで学ぶReact講座</h1>
+      <p>JSONPlaceholderのAPIを色々叩いてみるよ！</p>
     </div>
   );
 };
-export default About;
+export default Index;
