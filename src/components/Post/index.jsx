520c060cf50c2b91e365351c6c2da26828821311
hotarudesign <hotarudesign1220@gmail.com>
2023-07-31 09:29:15 +0900

    🎨 投稿取得周りのアーキテクチャが少し変更

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -6,7 +6,7 @@ import { usePost } from "src/hooks/usePost";
 
 export const Post = () => {
   const router = useRouter();
-  const { data, error, isLoading } = usePost();
+  const { data, error, isLoading } = usePost(router.query.id);
 
   if (isLoading) {
     return <div>ローディング中</div>;
